aedda85c52f63e9662ffbfd09da2ae7de50b789c
rohit nayyar <rohit@rohits-MacBook-Air.local>
2023-09-30 11:48:33 +0530

    Mongo error

--- Server/config/Connection.js
+++ Server/config/Connection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 
- mongoose.connect( "mongodb+srv://rohitnayyar54:160298Rn!@book-search-engine.povauw9.mongodb.net/ShopAll",{
+ mongoose.connect( "mongodb+srv://rohitnayyar54:160298rn!@book-search-engine.povauw9.mongodb.net/ShopAll",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 const db = mongoose.connection
 
 module.exports = db
+
 
 
