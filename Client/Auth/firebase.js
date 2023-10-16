0bc7859c2c6fed47babc45b7f632f142fe24d68b
rohit nayyar <rohit@rohits-MacBook-Air.local>
2023-10-16 13:13:32 -0400

    Added Firebase auth for Login/Signup

--- Client/Auth/firebase.js
+++ Client/Auth/firebase.js
@@ -2,15 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const app = initializeApp({
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
-  measurementId: import.meta.env.VITE_MEASURMENTID,
+  apiKey:process.env.VITE_APIKEY,
+  authDomain:process.env.VITE_AUTHDOMAIN,
+  projectId:process.env.VITE_PROJECTID,
+  storageBucket:process.env.VITE_STORAGEBUCKET,
+  messagingSenderId:process.env.VITE_MESSAGINGSENDERID,
+  appId:process.env.VITE_APPID,
+  measurementId:process.env.VITE_MEASURMENTID,
 });
 
 // export const auth = app.auth()
 export const authentication = getAuth(app);
 export default app;
+
+
+
+// apiKey: import.meta.env.VITE_APIKEY,
+// authDomain: import.meta.env.VITE_AUTHDOMAIN,
+// projectId: import.meta.env.VITE_PROJECTID,
+// storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+// messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+// appId: import.meta.env.VITE_APPID,
+// measurementId: import.meta.env.VITE_MEASURMENTID,
