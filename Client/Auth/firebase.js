5ac26c0d64b5a1420fe954424e0b00b396099cca
rohit nayyar <rohit@rohits-MacBook-Air.local>
2023-10-16 13:22:38 -0400

    Added Firebase auth for Login

--- Client/Auth/firebase.js
+++ Client/Auth/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const app = initializeApp({
-  apiKey:process.env.VITE_APIKEY,
-  authDomain:process.env.VITE_AUTHDOMAIN,
-  projectId:process.env.VITE_PROJECTID,
-  storageBucket:process.env.VITE_STORAGEBUCKET,
-  messagingSenderId:process.env.VITE_MESSAGINGSENDERID,
-  appId:process.env.VITE_APPID,
-  measurementId:process.env.VITE_MEASURMENTID,
+apiKey: "AIzaSyATPhOlqKTK9haJVTun6o4Rl2K7R-wQeic",
+authDomain: import.meta.env.VITE_AUTHDOMAIN,
+projectId: import.meta.env.VITE_PROJECTID,
+storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+appId: import.meta.env.VITE_APPID,
+measurementId: import.meta.env.VITE_MEASURMENTID,
 });
 
 // export const auth = app.auth()
